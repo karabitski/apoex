adaba3951020aa9517e992aa295ac9f2accd50c5
karabitski <a.karabitski@gmail.com>
2012-04-10 21:45:33 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apoex::Application.config.secret_token = '9f3711d3053af27b55c863be86c492b813962aa54b67d73cdb10786a18dba4b74617138845869caa711da8a42ad83a5f11d1a599edbdfb0fa4aeb6e33695fe66'

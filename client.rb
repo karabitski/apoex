ee6d12ddd74e27ee85dbfcec621a8866cf4c370e
karabitski <a.karabitski@gmail.com>
2012-04-11 23:53:05 +0300

    created class for console client script

--- client.rb
+++ client.rb
@@ -2,13 +2,25 @@
 require 'net/http'
 require 'json'
 
-BASE_URL = "http://localhost:3001?query="
+class Client
+  BASE_URL = "http://localhost:3001?query="
+  
+  def initialize(args = ARGV.join(' '))
+    @query = URI.encode args
+  end
+  
+  def query
+    JSON.parse(response.body)['reg_number']  
+  end
+  
+  def response
+    uri = URI.parse(BASE_URL + @query)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['accept'] = 'application/json'
+    http.request(request)  
+  end
+end
 
-uri = URI.parse(BASE_URL + URI.encode(ARGV.join(' ')))
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-request['accept'] = 'application/json'
-response = http.request(request)
-reg_number = JSON.parse(response.body)['reg_number']
-
-puts reg_number
+client = Client.new
+puts client.query
